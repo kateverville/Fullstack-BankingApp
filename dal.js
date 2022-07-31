42b61bfb867074ecd6e6076d77aac62e9d1cb1c8
kateverville <93614446+kateverville@users.noreply.github.com>
2022-07-31 18:51:58 -0500

    Renamed db

--- dal.js
+++ dal.js
@@ -1,14 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
-let db            = null;
- 
-// connect to mongo
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-    console.log("Connected successfully to db server");
+const url = 'mongodb+srv://admin:thirdtimesacharm@cluster0.fivdwwm.mongodb.net/?retryWrites=true&w=majority';
 
-    // connect to myproject database
-    db = client.db('myproject');
-});
+const client = new MongoClient(url);
+const dbName = "to-do-list";
+let db;
+let collection;
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  db = client.db(dbName);
+  collection = db.collection("tasks");
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error);
 
 // create user account
 function create(name, email, password){
